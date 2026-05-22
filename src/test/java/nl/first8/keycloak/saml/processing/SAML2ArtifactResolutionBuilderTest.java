b918c9204cea38686896e9366c81b6db1699a95a
oadham <omar.adham@first8.nl>
2026-05-22 15:02:13 +0200

    More test classes: SAMLUtil.java, SAMLEndpointTest (testHandleSamlResponse) and SAMLDataMarshallerTest; minor improvements to other tests

--- src/test/java/nl/first8/keycloak/saml/processing/SAML2ArtifactResolutionBuilderTest.java
+++ src/test/java/nl/first8/keycloak/saml/processing/SAML2ArtifactResolutionBuilderTest.java
@@ -19,20 +19,16 @@ class SAML2ArtifactResolutionBuilderTest {
 
     @Test
     void testToDocument_ValidInput_ShouldGenerateDocument() {
-        // Arrange
         String testArtifact = "testArtifact123";
         String testIssuer = "testIssuer";
 
         builder.artifact(testArtifact).issuer(testIssuer);
 
-        // Act
         Document document = builder.toDocument();
 
-        // Assert
         assertNotNull(document, "Generated Document should not be null");
         String xmlString = DocumentUtil.asString(document);
         assertTrue(xmlString.contains(testArtifact), "Artifact should be present in the XML");
         assertTrue(xmlString.contains(testIssuer), "Issuer should be present in the XML");
     }
 }
-
