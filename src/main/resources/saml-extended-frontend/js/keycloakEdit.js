dcc4718416100de425e630a68ac2aeedee9c5765
oadham <omar.adham@first8.nl>
2026-05-28 16:58:21 +0200

    Revert "Renamed wrongly capitalized serverUrl"
    This reverts commit facc1ed4a63159ab284d7e9bcaba2ca3d22932ce.

--- src/main/resources/saml-extended-frontend/js/keycloakEdit.js
+++ src/main/resources/saml-extended-frontend/js/keycloakEdit.js
@@ -1,6 +1,6 @@
 var accessToken;
 const keycloak = new Keycloak({
-  url: `${serverUrl}`,
+  url: `${ServerUrl}`,
   realm: `${realm}`,
   clientId: `${clientid}`,
   redirectUri: `${redirectUri}`,
@@ -9,7 +9,7 @@ const keycloak = new Keycloak({
 });
 
 document.getElementById('logout').addEventListener('click', () => {
-  window.location.href = `${serverUrl}/realms/${realm}/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
+  window.location.href = `${ServerUrl}/realms/${realm}/protocol/openid-connect/logout?post_logout_redirect_uri=${postLogoutRedirect}&client_id=${clientid}`;
 });
 
 keycloak
@@ -24,7 +24,7 @@ keycloak
       const selectElement_firstLoginFlow = document.getElementById('firstLoginFlow');
       var selectedrealm = localStorage.getItem('selectedRealm');
 
-      fetch(`${serverUrl}/admin/realms/${selectedrealm}/ui-ext/authentication-management/flows`, {
+      fetch(`${ServerUrl}/admin/realms/${selectedrealm}/ui-ext/authentication-management/flows`, {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${accessToken}`,
@@ -91,7 +91,7 @@ keycloak
       } else {
 
         alert("User does not have admin role. Access denied.");
-        window.location.href = `${serverUrl}/realms/${realm}/protocol/openid-connect/logout?post_logout_redirect_uri=${post_logout_redirect_uri}&client_id=${clientid}`;
+        window.location.href = `${ServerUrl}/realms/${realm}/protocol/openid-connect/logout?post_logout_redirect_uri=${post_logout_redirect_uri}&client_id=${clientid}`;
 
 
       }
